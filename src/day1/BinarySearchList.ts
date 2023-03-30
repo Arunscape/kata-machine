6e4cf3d3a0ff1e1378fced1bc7865550bb6b98ec
Arunscape <8227297+Arunscape@users.noreply.github.com>
2023-03-29 22:04:50 -0600

    binary search ezpz

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,17 @@
+// assume it's ordered
 export default function bs_list(haystack: number[], needle: number): boolean {
+ 
+  if (haystack.length === 0)
+    return false
 
+  const i = Math.floor(haystack.length/2)
+  const mid = haystack[i]
+
+  console.log(haystack, mid)
+  if (mid === needle)
+    return true
+  else if (needle < mid)
+    return bs_list(haystack.slice(0, i), needle)
+  else
+    return bs_list(haystack.slice(i+1, haystack.length), needle)
 }
